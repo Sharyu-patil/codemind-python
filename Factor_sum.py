fe420712b67117653eb01c1d7459b4a22617188d
Sharyu-patil <100119880+Sharyu-patil@users.noreply.github.com>
2022-10-11 21:19:26 +0530

    def fact(n):     r=0     for i in range(1,n+1):         if n%i==0:             r=r+i     return r
    a=list(map(int,input().split(',')))
    b=[]
    for i in a:
        s=fact(i)
        if s in a:
            b.append(i)
    if b==[]:
        print("-1")
    else:
        b.sort()
        print(*b)

---
+++ Factor_sum.py
@@ -0,0 +1,18 @@
+def fact(n):
+    r=0
+    for i in range(1,n+1):
+        if n%i==0:
+            r=r+i
+    return r
+
+a=list(map(int,input().split(',')))
+b=[]
+for i in a:
+    s=fact(i)
+    if s in a:
+        b.append(i)
+if b==[]:
+    print("-1")
+else:
+    b.sort()
+    print(*b)
