43606ae99fdd7f6b4249bbd20f25eeee1aa82c40
Sharyu-patil <100119880+Sharyu-patil@users.noreply.github.com>
2022-12-05 15:03:13 +0530

    n=int(input()) a=list(map(str,input().split())) a1=[] a2=[] for i in a:     a1.append(len(i)) for i in range(len(a1)):     if(a1[i]==min(a1)):         a2.append(a1[i]) print(len(a2))

--- Minimum_digit_elements.py
+++ Minimum_digit_elements.py
@@ -1,15 +1,10 @@
 n=int(input())
 a=list(map(str,input().split()))
-c=0
-count=0
-k=0
-minn=999
-x=[]
-for i in a:
-    x.append(len(i))
 a1=[]
-for i in range(len(x)):
-    if(x[i]==min(x)):
-        a1.append(x[i])
-        
-print(len(a1))
+a2=[]
+for i in a:
+    a1.append(len(i))
+for i in range(len(a1)):
+    if(a1[i]==min(a1)):
+        a2.append(a1[i])
+print(len(a2))
